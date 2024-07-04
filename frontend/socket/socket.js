1dc780cf3b4394bfc986bb0b99dcc80fcd357815
Ammar Haider <ammar.haider@promotheanventures.net>
2024-07-04 01:37:54 -0400

    aws elastic beanstalk setup

--- frontend/socket/socket.js
+++ frontend/socket/socket.js
@@ -2,13 +2,12 @@ import { io } from "socket.io-client";
 
 const IP_EDUROAM = "149.43.107.35";
 const IP = "192.168.1.233";
-const AWS_ADRESS = "http://ec2-3-19-221-76.us-east-2.compute.amazonaws.com";
+const AWS_ADRESS =
+  "http://7-chat-env2-env.eba-zpjcegve.us-east-2.elasticbeanstalk.com";
 const PORT = "4000";
 
 const setConnection = () => {
-  const socket = io(`http://${IP_EDUROAM}:${PORT}`, {
-    path: "/api/socket",
-  });
+  const socket = io(`${AWS_ADRESS}`);
   return socket;
 };
 
