43ced0400532b406f240cbd23ae0f64394c21aea
Ammar Haider <ammar.haider@promotheanventures.net>
2024-07-03 21:42:39 -0400

    after aws setup

--- frontend/socket/socket.js
+++ frontend/socket/socket.js
@@ -1,13 +1,13 @@
 import { io } from "socket.io-client";
 
 const IP_EDUROAM = "149.43.107.35";
-const IP = "192.168.1.233"
-
+const IP = "192.168.1.233";
+const AWS_ADRESS = "http://ec2-3-19-221-76.us-east-2.compute.amazonaws.com";
 const PORT = "4000";
 
 const setConnection = () => {
-    const socket = io(`http://${IP_EDUROAM}:${PORT}`);
-    return socket;
-}
+  const socket = io(`${AWS_ADRESS}:${PORT}`);
+  return socket;
+};
 
 export default setConnection;
